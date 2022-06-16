5f66938da7136826daf4c5871092b113262dc189
Ytalo Campos <ytalo.campos0@gmail.com>
2022-06-16 14:37:47 -0300

    feat/test: adds import and adds unique id

--- tests/domain/company/service/update.service.spec.ts
+++ tests/domain/company/service/update.service.spec.ts
@@ -1,9 +1,12 @@
 import { Company } from "@/domain/company/entity";
 import { UpdateCompany, updateCompanyService } from "@/domain/company/service";
 import { Address } from "@/domain/company/value-object";
+import { UniqueIdGenerator } from "@/domain/interface/gateways";
+import { UniqueIdGeneratorGateway } from "@/infrastructure/gateways";
 
 describe("UpdateCompany", () => {
   let updateCompany: UpdateCompany;
+  let uniqueIdGenerator: UniqueIdGenerator;
   const addressData = {
     city: "any_city",
     complement: "any_complement",
@@ -16,15 +19,16 @@ describe("UpdateCompany", () => {
 
   beforeEach(() => {
     updateCompany = updateCompanyService();
+    uniqueIdGenerator = new UniqueIdGeneratorGateway();
   });
 
-  it("should to call with correct input", () => {
+  it("should to call this service", () => {
     const address = new Address(addressData);
     const companyData = {
       address: address,
       cnpj: "any_cnpj",
       corporateName: "any_corporate_name",
-      id: "any_id",
+      id: uniqueIdGenerator.uuidv4(),
       logo: "any_logo",
       name: "any_name",
     };
