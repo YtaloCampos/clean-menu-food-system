7c1eab0ee5cb3a88da32b83ea3070a4ce9a1905f
Ytalo Campos <ytalo.campos0@gmail.com>
2022-06-17 13:43:52 -0300

    feat: adds save company repository interface

---
+++ src/domain/interface/repositories/save-company.interface.ts
@@ -0,0 +1,22 @@
+export interface SaveCompany {
+  save: (input: SaveCompany.Input) => void;
+}
+
+export namespace SaveCompany {
+  export type Input = {
+    id: string;
+    name: string;
+    corporateName: string;
+    cnpj: string;
+    logo: string;
+    address: {
+      zipCode: number;
+      houseNumber: number;
+      street: string;
+      complement: string;
+      neighborhood: string;
+      city: string;
+      state: string;
+    };
+  };
+}
