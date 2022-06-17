7daf6c40822e97c58505f88e08bdf73da73cf78f
Ytalo Campos <ytalo.campos0@gmail.com>
2022-06-17 17:25:13 -0300

    feat: impl get company service in update company service

--- src/domain/company/service/update.service.ts
+++ src/domain/company/service/update.service.ts
@@ -1,12 +1,22 @@
 import { Company } from '@/domain/company/entity';
 import { SaveCompany } from '@/domain/interface/repositories';
+import { GetCompany } from './get.service';
 
-type Setup = (companyRepository: SaveCompany) => UpdateCompany;
+type Setup = (
+  companyRepository: SaveCompany,
+  getCompany: GetCompany,
+) => UpdateCompany;
 type Input = Company;
 export type UpdateCompany = (input: Input) => void;
 
 export const updateCompanyService: Setup =
-  (companyRepository) => async (input) => {
+  (companyRepository, getCompany) => async (input) => {
+    const company = await getCompany({ id: input.id });
+
+    if (!company) {
+      throw new Error('Company not found');
+    }
+
     await companyRepository.save(input).catch(() => {
       throw new Error('Update company error');
     });
