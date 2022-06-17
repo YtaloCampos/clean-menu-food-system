097cc10fef71a8b50e2db4d6326e7c63220a96df
Ytalo Campos <ytalo.campos0@gmail.com>
2022-06-17 17:24:46 -0300

    feat: impl get company repository in get company service

--- src/domain/company/service/get.service.ts
+++ src/domain/company/service/get.service.ts
@@ -1,7 +1,12 @@
-type Setup = () => GetCompany;
-type Input = { id: string };
-export type GetCompany = (input: Input) => void;
+import { LoadCompany } from '@/domain/interface/repositories';
 
-export const getCompanyService: Setup = () => (input) => {
-  // TODO
+type Setup = (loadCompany: LoadCompany) => GetCompany;
+type Input = { id: string };
+type OutPut = LoadCompany.OutPut;
+export type GetCompany = (input: Input) => Promise<OutPut>;
+
+export const getCompanyService: Setup = (loadCompany) => async (input) => {
+  return await loadCompany.load({ id: input.id }).catch(() => {
+    throw new Error('Get company error');
+  });
 };
