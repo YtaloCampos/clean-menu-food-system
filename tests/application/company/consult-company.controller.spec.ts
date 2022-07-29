7893b93de843802974ab001e7a707636cd1fc05d
Ytalo Campos <ytalo.campos0@gmail.com>
2022-07-29 13:14:08 -0300

    test(company): init company controller tests

---
+++ tests/application/company/consult-company.controller.spec.ts
@@ -0,0 +1,97 @@
+import { ConsultCompanyController } from '@/application/company/controller';
+import { Company } from '@/domain/company/entity';
+import { Address } from '@/domain/company/value-object';
+
+describe('ConsultCompanyController', () => {
+  let consultCompanyController: ConsultCompanyController;
+  let consultCompany: jest.Mock;
+  const companyData = new Company({
+    id: 'any_id',
+    name: 'any_name',
+    corporateName: 'any_corporate_name',
+    cnpj: 'any_cnpj',
+    logo: 'any_logo',
+    address: new Address({
+      zipCode: 1,
+      houseNumber: 1,
+      street: 'any_address_street',
+      complement: 'any_address_complement',
+      neighborhood: 'any_address_neighborhood',
+      city: 'any_city',
+      state: 'any_street',
+    }),
+  });
+
+  beforeAll(() => {
+    consultCompany = jest.fn();
+    consultCompany.mockResolvedValue(companyData);
+  });
+
+  beforeEach(() => {
+    consultCompanyController = new ConsultCompanyController(consultCompany);
+  });
+
+  it('Should return 400 if companyId is empty', async () => {
+    const httpResponse = await consultCompanyController.handle({
+      companyId: '',
+    });
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new Error('The field companyId is required'),
+    });
+  });
+
+  it('Should return 400 if id is null', async () => {
+    const httpResponse = await consultCompanyController.handle({ id: null });
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new Error('The field companyId is required'),
+    });
+  });
+
+  it('Should return 400 if id is undefined', async () => {
+    const httpResponse = await consultCompanyController.handle({
+      id: undefined,
+    });
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new Error('The field companyId is required'),
+    });
+  });
+
+  it('Shoud call consult company with correct params', async () => {
+    await consultCompanyController.handle({
+      id: 'any_id',
+    });
+
+    expect(consultCompany).toHaveBeenCalledWith({
+      id: 'any_id',
+    });
+  });
+
+  it('Should return 200 if consult company succeds', async () => {
+    const httpResponse = await consultCompanyController.handle({
+      id: 'any_id',
+    });
+
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      data: companyData,
+    });
+  });
+
+  it('Should return 500 if consult company throws', async () => {
+    consultCompany.mockRejectedValueOnce(new Error('Consult company fails'));
+    const httpResponse = await consultCompanyController.handle({
+      id: 'any_id',
+    });
+
+    expect(httpResponse).toEqual({
+      statusCode: 500,
+      data: new Error('Consult company fails'),
+    });
+  });
+});
